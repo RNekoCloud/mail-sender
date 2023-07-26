4034bb222d88c6b914a1130772e77e516ea95f6e
RageNeko26 <rneko2006@gmail.com>
2023-07-26 15:31:05 +0700

    feat: Setup SMTP config

--- send.go
+++ send.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var HOST string
+var PORT string
+var USERNAME string
+var PASSWORD string
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic(err)
+	}
+
+	HOST = os.Getenv("SMTP_HOST")
+	PORT = os.Getenv("SMTP_PORT")
+	USERNAME = os.Getenv("SMTP_USERNAME")
+	PASSWORD = os.Getenv("SMTP_PASSWORD")
+
+}
